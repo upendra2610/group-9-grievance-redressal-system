db112690e7d6af2dd76e6877e060194f9648fc61
brohit-97 <rohitbarvekar97@gmail.com>
2023-10-28 12:12:32 +0530

    comments resolved

--- grievance-management-service/src/main/java/com/scaler/grievancemanagementservice/controllers/GrievanceOperations.java
+++ grievance-management-service/src/main/java/com/scaler/grievancemanagementservice/controllers/GrievanceOperations.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@RequestMapping("/api/v1/grievances")
+@RequestMapping("/grievances")
 public interface GrievanceOperations {
 
     @GetMapping("/")
